65a79d9fadeb1254f38297e4258c0b91918a0f27
AnoPiS <omer.mkawy.om@gmail.com>
2024-07-17 03:08:47 +0300

    Done: charts {pie for must - need - want %}, todo: work on stat screen (line/column chart to show week-days / months sum trend})

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 import 'package:walletwize/shared/styles/themes.dart';
--- lib/modules/stats_screen.dart
+++ lib/modules/stats_screen.dart
@@ -1,7 +1,5 @@
-import 'package:d_chart/commons/data_model.dart';
 import 'package:d_chart/d_chart.dart';
-import 'package:d_chart/ordinal/pie.dart';
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
